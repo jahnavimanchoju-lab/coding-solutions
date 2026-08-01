a3f49bfccc40debccbc6e67b07b5b545e3199785
jahnavimanchoju-lab <jahnavi.manchoju@gmail.com>
2026-08-01 19:51:34 +0530

    ✅ Loops | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-01

--- hackerrank/medium/python-loops/solution.py
+++ hackerrank/medium/python-loops/solution.py
@@ -1,5 +1,6 @@
 if __name__ == '__main__':
-    a = int(input())
-    b = int(input())
-    print(a//b)
-    print(a/b)
+    n = int(input())
+    if n>0:
+        for i in range(n):
+            print(i*i)
+            
