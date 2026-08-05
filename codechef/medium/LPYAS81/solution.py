dcd48b7b00ec80348e00da9d74a589b07376d4bc
jahnavimanchoju-lab <jahnavi.manchoju@gmail.com>
2026-08-05 20:57:49 +0530

    ✅ LPYAS81 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-05

---
+++ codechef/medium/LPYAS81/solution.py
@@ -0,0 +1,8 @@
+# Given dictionary
+student_grades = {"Alice": 85, "Bob": 72, "Charlie": 90, "David": 65, "Eva": 88, "John": 45}
+name=input()
+# Complete the code 
+if name in student_grades:
+    print(student_grades[name])
+else:
+    print("Not Found")
