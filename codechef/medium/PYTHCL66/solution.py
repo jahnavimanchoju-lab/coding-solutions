0823ead222dfd95e9522a10742ad435a866c7cf5
jahnavimanchoju-lab <jahnavi.manchoju@gmail.com>
2026-08-12 21:01:35 +0530

    ✅ PYTHCL66 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-12

--- codechef/medium/PYTHCL66/solution.py
+++ codechef/medium/PYTHCL66/solution.py
@@ -1,7 +1,10 @@
-b = int(input())
-r = int(input())
-if r>b:
-    print("Rob scored higher marks than Bob")
-elif b==r:
-    print("Bob &Rob scored the same")
+# Update the blanks in the code below to solve the problem
 
+r, k = map(int, input().split())
+
+if r>k:
+    print("Ram is heavier than Karan")
+elif r<k:
+    print("Karan is heavier than Ram")
+else:
+    print("Ram & Karan have the same weight")
