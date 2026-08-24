1f37cc55dd88fbff97e659b90dac6347da6e9ae7
jahnavimanchoju-lab <jahnavi.manchoju@gmail.com>
2026-08-24 19:35:16 +0530

    ✅ PYTHCL113A | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-24

--- codechef/medium/PYTHCL113A/solution.py
+++ codechef/medium/PYTHCL113A/solution.py
@@ -1,10 +1,3 @@
-# Count how many 'o's are present in the string using a 'for' loop and 'if' condition
-
-string = 'bolloon'
-
-# use this variable to count occurrences of o
-count_o = 0 
-for i in string:
-    if i=='o':
-        count_o=count_o+1
-print(count_o)
+# Update the '_' to solve the problem
+for i in range(55, 100, 3):
+    print(i)
