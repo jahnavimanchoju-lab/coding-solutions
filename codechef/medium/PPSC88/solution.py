732490758b00d2e468652b345c14f2dcf3c6ce8b
jahnavimanchoju-lab <jahnavi.manchoju@gmail.com>
2026-08-27 20:04:07 +0530

    ✅ PPSC88 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-27

---
+++ codechef/medium/PPSC88/solution.py
@@ -0,0 +1,15 @@
+n = int(input())
+isPrime = True # Assume n is prime
+
+i = 2
+while i < n:
+    # Update your code below this line
+    if n%i==0:
+        isPrime=False
+    i=i+1
+    
+    
+if isPrime:
+    print('Yes')
+else:
+    print('No')
