756afce4955b522d359874ad40887ceb862c5e4b
jahnavimanchoju-lab <jahnavi.manchoju@gmail.com>
2026-09-11 19:59:40 +0530

    ✅ PSPP82 | Medium | Python | Runtime: N/A | Memory: N/A | 2026-09-11

---
+++ codechef/medium/PSPP82/solution.py
@@ -0,0 +1,19 @@
+def greet(name):
+    return f"Hello, {name}!"
+
+def capitalize(text):
+    return text.upper()
+
+def greet_and_capitalize(name):
+    greeting=greet(name)
+    return capitalize(greeting)
+
+
+
+
+# Call the functions
+name = "Alice"
+final_result = greet_and_capitalize(name)
+
+# Display the results
+print("Final Result:", final_result)
